f492e0c4bc7b1b973294928366b9f2c45960dad8
unknown <shubhabrata08@gmail.com>
2022-05-07 03:17:36 +0530

    Navigation setup

--- AIVideoUpscaler/AIVU/urls.py
+++ AIVideoUpscaler/AIVU/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home,name='home'),
+    path('upscale/', views.upscale,name='upscale'),
+    path('about/', views.about,name='about'),
 ]
--- AIVideoUpscaler/AIVU/views.py
+++ AIVideoUpscaler/AIVU/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'index.html')
+
+def upscale(request):
+    return render(request,'upscale.html')
+
+def about(request):
+    return render(request,'about.html')
